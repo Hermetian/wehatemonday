f3a3cf6eefac2939f07aaac3d6c951a7adce9d6d
Hermetian <cordwell@gmail.com>
2025-01-21 15:48:59 -0700

    Ticket Dialog (vlint 2)

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { User, Session } from '@supabase/supabase-js';
+import { User } from '@supabase/supabase-js';
 import { supabase, supabaseAdmin } from '../lib/supabase';
 import { UserRole } from '@prisma/client';
 
